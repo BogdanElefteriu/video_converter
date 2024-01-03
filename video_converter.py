883dfce1e53c10b447f8ad66e7a5c7e8cebe0bf0
Elefteriu Bogdan <be@Elefterius-MacBook-Pro.local>
2024-01-03 19:37:03 +0200

    Brought modifications to the video saving after import.

--- video_converter.py
+++ video_converter.py
@@ -11,7 +11,14 @@ def convert_to_mp4(file_path):
 
     clip = moviepy.VideoFileClip(file_path)
     video_with_new_audio = clip.set_audio(moviepy.AudioFileClip(file_path)) 
-    video_with_new_audio.write_videofile(f"{file_path.split('.')[0]}.mp4", fps=60, audio_codec="aac")
+    video_with_new_audio.write_videofile(f"{file_path.split('.')[0]}.mp4",
+                                        bitrate='8000k',
+                                        ffmpeg_params=[
+                                            '-tile-columns', '6', '-frame-parallel', '0',
+                                            '-auto-alt-ref', '1', '-lag-in-frames', '25', '-g',
+                                            '128', '-pix_fmt', 'yuv420p', '-row-mt', '1']
+                                        )
+
 
 if not "temp_file" in st.session_state:
     st.session_state['temp_file'] = ''
@@ -25,9 +32,9 @@ if not "convert" in st.session_state:
 
 st.title("Conversie video la formatul MP4")
 
-    
+
 file = st.file_uploader(label="Uploadeaza fisierul")
-temporary_location = "temp_conversion.mp4"
+temporary_location = "temp_conversion" + "." + file.name.split(".")[1]
 
 if file:
     st.write(f"Ai incarcat fisierul: {file.name}")
@@ -37,14 +44,15 @@ if file:
     convert = st.button("Converteste video-ul")
     if st.session_state.download:
         os.remove(temporary_location)
+        os.remove(temporary_location.split('.')[0]+".mp4")
         st.session_state.download = False
 
     if convert:
         st.session_state.convert = True
-        g = io.BytesIO(file.read())  ## BytesIO Object
+        # g = io.BytesIO(file.getvalue())  ## BytesIO Object
     
         with open(temporary_location, 'wb') as out:  ## Open temporary file as bytes
-            out.write(g.read())  ## Read bytes into file
+            out.write(file.getbuffer())  ## Read bytes into file
 
         # close file
         out.close()
@@ -56,13 +64,13 @@ if file:
 tabs =  st.tabs(["Video convertit"])
 with tabs[0]:
     if type(file) is not type(None):
-        if os.path.exists(temporary_location):
+        if os.path.exists(temporary_location.split('.')[0]+".mp4"):
             if st.session_state.convert:
-                st.video(temporary_location)
+                st.video(temporary_location.split('.')[0]+".mp4")
 
                 if st.download_button(label = "Descarca video-ul",
                                         file_name=f"{file.name.split('.')[0]}.mp4",
-                                        data = open(temporary_location, "rb")):
+                                        data = open(temporary_location.split('.')[0]+".mp4", "rb")):
                     st.session_state.download = True
                     st.rerun()
 
